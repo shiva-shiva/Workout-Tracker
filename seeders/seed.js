235a29ed3db237666e2afeb0b9b1f79ce594e6c2
shiva sabokdast <shiva.sabokdast@gmail.com>
2021-03-11 22:37:44 -0500

    deploying

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,10 +1,16 @@
 let mongoose = require("mongoose");
 let db = require("../models");
+require('dotenv').config();  
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 let workoutSeed = [
   {
